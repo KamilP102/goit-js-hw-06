5459e0b2f40d51be14086e118628c9aa4c4d4eb3
KamilP102 <kamilp1319@gmail.com>
2023-01-06 13:51:19 +0100

    Update task-06.js

---
+++ src/js/task-06.js
@@ -0,0 +1,13 @@
+const inputEl = document.querySelector("#validation-input");
+
+const makeChange = () => {
+    if (inputEl.value.length < 6) {
+        inputEl.classList.add("invalid")
+        inputEl.classList.remove("valid")
+    } else {
+        inputEl.classList.remove("invalid")
+        inputEl.classList.add("valid")
+    }
+};
+
+inputEl.addEventListener("blur", makeChange);
