29af05ae98292280a75128a764aa970eec05de0c
KamilP102 <kamilp1319@gmail.com>
2023-01-07 20:12:41 +0100

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
+const changeColor = document.querySelector(".change-color")
+const background = document.querySelector("body")
+const color = document.querySelector(".color")
+
+const makeChange = (e) => {
+  background.style.backgroundColor = getRandomHexColor();
+  color.innerHTML = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+changeColor.addEventListener("click", makeChange)
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
