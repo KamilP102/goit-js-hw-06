bc81ada1db6c1292a69577a2c2b8a2f48dac5a58
KamilP102 <kamilp1319@gmail.com>
2023-01-07 19:17:53 +0100

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,16 +1,17 @@
 const form = document.querySelector(".login-form");
 
+
 function handelSubmit(event) {
     event.preventDefault();
     const {
-        elements: { email,password }
+        elements: { email, password }
     } = event.currentTarget;
 
-    if (email.value === " " || password.value === " ") {
-    return alert("All fields should be filled!!!");
+    if (email.value === "" || password.value === "") {
+        return alert("All fields should be filled!!!");
     }
-    console.log(`Login: ${email.value}\nPassword: ${password.value}`);
-    event.currentTarget.reset();
+    const user = { email: email.value, password: password.value };
+    console.log(user);
+    event.currentTarget.reset();  
 }
-
 form.addEventListener("submit", handelSubmit);
