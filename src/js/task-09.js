0f6c37ab87bde420644c3938759857c0615c1b9f
KamilP102 <kamilp1319@gmail.com>
2023-01-24 18:11:53 +0100

    Update task 09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,16 +1,13 @@
 const changeColor = document.querySelector(".change-color")
-const background = document.querySelector("body")
+const body = document.querySelector("body")
 const color = document.querySelector(".color")
 
-const makeChange = (e) => {
-  background.style.backgroundColor = getRandomHexColor();
-  color.innerHTML = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const makeChange = () => {
+  color.innerHTML = getRandomHexColor()
+  body.style.backgroundColor = color.innerHTML;
 }
 
-
 changeColor.addEventListener("click", makeChange)
-
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
